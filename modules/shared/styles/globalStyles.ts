f46bfd75d1cbd278e7ce77c7dafa35d9cd8325c0
Jesus Imitola <47477274+jgimitola@users.noreply.github.com>
2024-05-19 13:39:54 -0500

    feat: configure Tanstack query and install another libraries

---
+++ modules/shared/styles/globalStyles.ts
@@ -0,0 +1,15 @@
+import { css } from "@emotion/react";
+
+const globalStyles = css`
+  html,
+  body,
+  #__next {
+    height: 100%;
+    max-height: 100%;
+    
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+export default globalStyles;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,66 @@
-import type { AppProps } from "next/app";
+import { AppProps } from "next/app";
+import Head from "next/head";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import { CacheProvider, EmotionCache, Global } from "@emotion/react";
+
+import {
+  HydrationBoundary,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+
+import createEmotionCache from "@/shared/lib/createEmotionCache";
+import globalStyles from "@/shared/styles/globalStyles";
+import theme from "@/shared/styles/theme";
+
+const clientSideEmotionCache = createEmotionCache();
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+      retryOnMount: false,
+    },
+  },
+});
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+export default function MyApp(props: MyAppProps) {
+  const {
+    Component,
+    emotionCache = clientSideEmotionCache,
+    pageProps: { session: _session, ...pageProps },
+  } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <title>Base Template</title>
+        <meta name="description" content="Base Template" />
+
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+      <Global styles={globalStyles} />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+
+        <QueryClientProvider client={queryClient}>
+          <HydrationBoundary state={pageProps.dehydratedState}>
+            <Component {...pageProps} />
+          </HydrationBoundary>
+
+          <ReactQueryDevtools initialIsOpen={false} position="right" />
+        </QueryClientProvider>
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
